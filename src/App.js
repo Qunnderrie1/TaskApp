9f40da2e7026dac0faacfb5f7eaa982713100806
qunnderrie1 <qunnderrie@gmail.com>
2023-12-07 22:27:06 -0500

    made some changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Component/Home";
 import CompletedTasks from "./Component/CompletedTasks";
 import NavBar from "./Component/NavBar";
+import NotFound404 from "./Component/NotFound404";
 function App() {
   return (
     <div className="App container-fluid">
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tasks/completed" element={<CompletedTasks />} />
+        <Route path="*" element={<NotFound404 />} />
       </Routes>
     </div>
   );
